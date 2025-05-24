0aa2a6a04b5dbbf6a0ab12ae9e1fb52d5ad1df37
alishba <alishbarehman048@gmail.com>
2025-05-24 14:59:36 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/currency-exchange-react/',
+  plugins: [react(),
+   tailwindcss() ],
+  })
